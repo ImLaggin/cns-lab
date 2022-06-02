333e07e937e3799a8f2ce189f0ee63cb549b9f41
necrosis2011 <100788377+necrosis2011@users.noreply.github.com>
2022-06-02 11:02:02 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,9 +1,22 @@
 plainText = input("ENTER THE PLAINTEXT ").upper()
 key = input("ENTER THE KEY ").upper()
+cipherText = []
+decryptedText = []
+
 temp = key
 i = 0
 while(len(key) < len(plainText)):
     key += temp[i]
     i = (i + 1) % len(temp)
+    
 for i, j in zip(plainText, key):
-    print(chr(((ord(i) % 65) + (ord(j) % 65)) % 26 + 65))
+    cipherText.append(chr(((ord(i) % 65) + (ord(j) % 65)) % 26 + 65))
+    
+cipherText = str("".join(cipherText))
+print(cipherText)
+
+for i, j in zip(cipherText, key):
+    decryptedText.append(chr(((ord(i) % 65) - (ord(j) % 65)) % 26 + 65))
+    
+decryptedText = str("".join(decryptedText))
+print(decryptedText)
