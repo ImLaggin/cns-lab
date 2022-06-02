5ebd562250074adc8325e5f0b97b48789d50f525
necrosis2011 <100788377+necrosis2011@users.noreply.github.com>
2022-06-02 10:55:28 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -57,6 +57,16 @@ def playFair(c1, c2):
     else:
         return keyMat[row[c1]][col[c2]]+keyMat[row[c2]][col[c1]]
 
+def playFair2(c1, c2):
+    if(row[c1] == row[c2]):
+        t1, t2 = col[c1], col[c2]
+        return keyMat[row[c1]][(t1-1) % 5]+keyMat[row[c2]][(t2-1) % 5]
+    elif(col[c1] == col[c2]):
+        t1, t2 = row[c1], row[c2]
+        return keyMat[(t1-1) % 5][col[c1]]+keyMat[(t2-1) % 5][col[c2]]
+    else:
+        return keyMat[row[c1]][col[c2]]+keyMat[row[c2]][col[c1]]
+
 
 i = 0
 while(i < len(plainText)):
@@ -64,6 +74,53 @@ while(i < len(plainText)):
         ans += playFair(plainText[i], 'X')
     else:
         ans += playFair(plainText[i], plainText[i+1])
+        i += 1
+    i += 1
+print(ans)
+
+i, j = 0, 0
+for e in key:
+    if(visited[e] == 1):
+        continue
+    if(j < 4):
+        keyMat[i][j] = e
+        row[e] = i
+        col[e] = j
+        j += 1
+    else:
+        keyMat[i][j] = e
+        row[e] = i
+        col[e] = j
+        j = 0
+        i += 1
+    visited[e] = 1
+
+for e in alphabets:
+    if(e == 'J'):
+        continue
+    if(visited[e] == 1):
+        continue
+    if(j < 4):
+        keyMat[i][j] = e
+        row[e] = j
+        col[e] = i
+        j += 1
+    else:
+        keyMat[i][j] = e
+        row[e] = j
+        col[e] = i
+        j = 0
+        i += 1
+    visited[e] = 1
+    
+plainText = ans
+ans=''
+i = 0
+while(i < len(plainText)):
+    if(i == len(plainText)-1 or plainText[i] == plainText[i+1]):
+        ans += playFair2(plainText[i], 'X')
+    else:
+        ans += playFair2(plainText[i], plainText[i+1])
         i += 1
     i += 1
 print(ans)
