1d5cb9436cb56c54300d509abf91308bcb7bee3d
necrosis2011 <100788377+necrosis2011@users.noreply.github.com>
2022-06-15 17:11:24 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,14 +2,12 @@ keyMatrix = [[0] * 3 for i in range(3)]
 messageVector = [[0] for i in range(3)]
 cipherMatrix = [[0] for i in range(3)]
 
-
 def getKeyMatrix(key):
     k = 0
     for i in range(3):
         for j in range(3):
             keyMatrix[i][j] = ord(key[k]) % 65
             k += 1
-
 
 def encrypt(messageVector):
     for i in range(3):
@@ -19,7 +17,6 @@ def encrypt(messageVector):
                 cipherMatrix[i][j] += (keyMatrix[i][x] *
                                        messageVector[x][j])
             cipherMatrix[i][j] = cipherMatrix[i][j] % 26
-
 
 def HillCipher(message, key):
     getKeyMatrix(key)
@@ -33,7 +30,6 @@ def HillCipher(message, key):
         CipherText.append(chr(cipherMatrix[i][0] + 65))
 
     print("Ciphertext: ", "".join(CipherText))
-
 
 plainText = input("ENTER THE PLAIN TEXT ").upper()
 key = input("ENTER THE KEY ").upper()
