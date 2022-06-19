bcf1acd94532823291d5cf9ee23ded61944cf249
cronesis <100788377+cronesis@users.noreply.github.com>
2022-06-19 10:16:02 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,40 @@
-plainText = input("ENTER THE PLAIN TEXT").upper().replace(' ','')
-key = int(input("ENTER THE KEY"))
-encr = ''.join([chr(((ord(i)%65)+key)%26 + 65) for i in plainText])
-print(encr)
-decr = ''.join([chr(((ord(i)%65)-key)%26 + 65) for i in encr])
-print(decr)
+plainText = input("Enter the plaintext: ")
+key = int(input("Enter the key: "))
+if key > 26:
+    key %= 26
+
+cipherText = ''
+for char in plainText:
+    if char.isdigit():
+        char = chr((ord(char)%48+key)%10+48)
+        cipherText += char
+    elif char.isalpha():
+        if char.isupper():
+            char = chr((ord(char)%65+key)%26+65)
+            cipherText += char
+        else:
+            char = chr((ord(char)%97+key)%26+97)
+            cipherText += char
+    else:
+        cipherText += char
+        continue
+    
+print("Encrypted text is:" ,cipherText)
+
+decryptedText = ''
+for char in cipherText:
+    if char.isdigit():
+        char = chr((ord(char)%48-key)%10+48)
+        decryptedText += char
+    elif char.isalpha():
+        if char.isupper():
+            char = chr((ord(char)%65-key)%26+65)
+            decryptedText += char
+        else:
+            char = chr((ord(char)%97-key)%26+97)
+            decryptedText += char
+    else:
+        decryptedText += char
+        continue
+    
+print("Decrypted text is:" ,decryptedText)
