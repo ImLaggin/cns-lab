b3a3d41a7d9396e0ffa841a86676e13689fac86a
cronesis <100788377+cronesis@users.noreply.github.com>
2022-07-09 19:26:38 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,124 +1,103 @@
-plainText = input("ENTER THE PLAIN TEXT ").upper().replace('J', "I").replace(' ','')
-key = input("ENTER THE KEY ").upper().replace('J', 'I').replace(' ','')
+import string
 
-alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+inputString = input("Enter the input string: ").upper().replace(
+    'J', 'I').replace(' ', '')
+key = input("Enter the key: ").upper().replace('J', 'I').replace(' ', '')
+
+alphabets = string.ascii_uppercase
 visited = {i: 0 for i in alphabets}
-row = {}
-col = {}
-keyMat = [['a', 'a', 'a', 'a', 'a'] for i in range(5)]
+rowIndex = {}
+columnIndex = {}
+keyMatrix = [['a', 'a', 'a', 'a', 'a'] for i in range(5)]
 
-i, j = 0, 0
-for e in key:
-    if(visited[e] == 1):
-        continue
-    if(j < 4):
-        keyMat[i][j] = e
-        row[e] = i
-        col[e] = j
-        j += 1
-    else:
-        keyMat[i][j] = e
-        row[e] = i
-        col[e] = j
-        j = 0
-        i += 1
-    visited[e] = 1
 
-for e in alphabets:
-    if(e == 'J'):
-        continue
-    if(visited[e] == 1):
-        continue
-    if(j < 4):
-        keyMat[i][j] = e
-        row[e] = i
-        col[e] = j
-        j += 1
-    else:
-        keyMat[i][j] = e
-        row[e] = i
-        col[e] = j
-        j = 0
-        i += 1
-    visited[e] = 1
+def constructKeyMatrix():
+    i, j = 0, 0
+    for e in key:
+        if(visited[e] == 1):
+            continue
+        if(j < 4):
+            keyMatrix[i][j] = e
+            rowIndex[e] = i
+            columnIndex[e] = j
+            j += 1
+        else:
+            keyMatrix[i][j] = e
+            rowIndex[e] = i
+            columnIndex[e] = j
+            j = 0
+            i += 1
+        visited[e] = 1
 
-ans = ""
-for k in keyMat:
+    for e in alphabets:
+        if(e == 'J'):
+            continue
+        if(visited[e] == 1):
+            continue
+        if(j < 4):
+            keyMatrix[i][j] = e
+            rowIndex[e] = i
+            columnIndex[e] = j
+            j += 1
+        else:
+            keyMatrix[i][j] = e
+            rowIndex[e] = i
+            columnIndex[e] = j
+            j = 0
+            i += 1
+        visited[e] = 1
+
+
+constructKeyMatrix()
+for k in keyMatrix:
     print(k)
 
-def playFair(c1, c2):
-    if(row[c1] == row[c2]):
-        t1, t2 = col[c1], col[c2]
-        return keyMat[row[c1]][(t1+1) % 5]+keyMat[row[c2]][(t2+1) % 5]
-    elif(col[c1] == col[c2]):
-        t1, t2 = row[c1], row[c2]
-        return keyMat[(t1+1) % 5][col[c1]]+keyMat[(t2+1) % 5][col[c2]]
-    else:
-        return keyMat[row[c1]][col[c2]]+keyMat[row[c2]][col[c1]]
 
-def playFair2(c1, c2):
-    if(row[c1] == row[c2]):
-        t1, t2 = col[c1], col[c2]
-        return keyMat[row[c1]][(t1-1) % 5]+keyMat[row[c2]][(t2-1) % 5]
-    elif(col[c1] == col[c2]):
-        t1, t2 = row[c1], row[c2]
-        return keyMat[(t1-1) % 5][col[c1]]+keyMat[(t2-1) % 5][col[c2]]
+def playFair(c1, c2, encrypt=True):
+    if encrypt:
+        if(rowIndex[c1] == rowIndex[c2]):
+            t1, t2 = columnIndex[c1], columnIndex[c2]
+            return keyMatrix[rowIndex[c1]][(t1+1) % 5]+keyMatrix[rowIndex[c2]][(t2+1) % 5]
+        elif(columnIndex[c1] == columnIndex[c2]):
+            t1, t2 = rowIndex[c1], rowIndex[c2]
+            return keyMatrix[(t1+1) % 5][columnIndex[c1]]+keyMatrix[(t2+1) % 5][columnIndex[c2]]
+        else:
+            return keyMatrix[rowIndex[c1]][columnIndex[c2]]+keyMatrix[rowIndex[c2]][columnIndex[c1]]
     else:
-        return keyMat[row[c1]][col[c2]]+keyMat[row[c2]][col[c1]]
+        if(rowIndex[c1] == rowIndex[c2]):
+            t1, t2 = columnIndex[c1], columnIndex[c2]
+            return keyMatrix[rowIndex[c1]][(t1-1) % 5]+keyMatrix[rowIndex[c2]][(t2-1) % 5]
+        elif(columnIndex[c1] == columnIndex[c2]):
+            t1, t2 = rowIndex[c1], rowIndex[c2]
+            return keyMatrix[(t1-1) % 5][columnIndex[c1]]+keyMatrix[(t2-1) % 5][columnIndex[c2]]
+        else:
+            return keyMatrix[rowIndex[c1]][columnIndex[c2]]+keyMatrix[rowIndex[c2]][columnIndex[c1]]
 
-i = 0
-while(i < len(plainText)):
-    if(i == len(plainText)-1 or plainText[i] == plainText[i+1]):
-        ans += playFair(plainText[i], 'X')
-    else:
-        ans += playFair(plainText[i], plainText[i+1])
-        i += 1
-    i += 1
-print(ans)
 
-i, j = 0, 0
-for e in key:
-    if(visited[e] == 1):
-        continue
-    if(j < 4):
-        keyMat[i][j] = e
-        row[e] = i
-        col[e] = j
-        j += 1
-    else:
-        keyMat[i][j] = e
-        row[e] = i
-        col[e] = j
-        j = 0
-        i += 1
-    visited[e] = 1
+def driver(choice=1):
+    resultString = ""
+    i = 0
 
-for e in alphabets:
-    if(e == 'J'):
-        continue
-    if(visited[e] == 1):
-        continue
-    if(j < 4):
-        keyMat[i][j] = e
-        row[e] = j
-        col[e] = i
-        j += 1
-    else:
-        keyMat[i][j] = e
-        row[e] = j
-        col[e] = i
-        j = 0
-        i += 1
-    visited[e] = 1
-    
-plainText = ans
-ans=''
-i = 0
-while(i < len(plainText)):
-    if(i == len(plainText)-1 or plainText[i] == plainText[i+1]):
-        ans += playFair2(plainText[i], 'X')
-    else:
-        ans += playFair2(plainText[i], plainText[i+1])
-        i += 1
-    i += 1
-print(ans)
+    while(i < len(inputString)):
+        if choice == 1:
+            if(i == len(inputString)-1 or inputString[i] == inputString[i+1]):
+                resultString += playFair(inputString[i], 'X')
+            else:
+                resultString += playFair(inputString[i],
+                                         inputString[i+1])
+                i += 1
+            i += 1
+        elif choice == 2:
+            if(i == len(inputString)-1 or inputString[i] == inputString[i+1]):
+                resultString += playFair(inputString[i], 'X', encrypt=False)
+            else:
+                resultString += playFair(inputString[i],
+                                         inputString[i+1], encrypt=False)
+                i += 1
+            i += 1
+
+    print("Resultant string is:", resultString)
+
+
+choice = int(input("1. Encrypt\n2. Decrypt\n"))
+driver(choice)
